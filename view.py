37a67705ca9fb54835529e89a166b2b40b951a63
abatuFF <nurbekuuludaniel@gmail.com>
2021-08-18 19:45:57 +0600

    add css styles

--- view.py
+++ view.py
@@ -3,21 +3,32 @@ from flask import render_template
 
 @app.route('/')
 def index():
-	return '<h1>Hello world</h1>'
+	f = open('count.txt','r')
+	count=int(f.read())
+	f.close()
 
+	count += 1
 
-@app.route('/second')
-def second():
-	context ={
-	'name':'Kazahstan',
-	'age':'30',
-	'gender':'male',
-	}
+	f=open('count.txt','w')
+	f.write(str(count))
+	f.close()
+
+	return render_template('app/index.html', count=count)
 
 @app.route('/view')
 def third():
 	context={
 		'languages':'python,css,html,ruby,JS',
 	}
-	
-	return render_template('app/index.html', **context)
+	return render_template('app/main.html', **context)
+
+@app.route('/1')
+def second():
+	posts= ['clown', 'ruck and shmorti', 'anarhi sons']
+	context={
+	'name':'Danya',
+	'age':'500',
+	'gender':'man',
+	"posts":posts
+	}
+	return render_template('app/main.html', **context)
